a243b01ece8a61bf4d74771d91a49a5c5844e80c
Al Stone <astone@heckgrammar.co.uk>
2025-11-11 11:16:59 +0000

    Add project files.

---
+++ StringHandling9D25/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringHandling9D25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a string variable called input
+            string input;
+            // ask the user to enter a value
+            Console.Write("Enter something: ");
+            // store the input in the variable input
+            input =Console.ReadLine();
+            // output the value to the console
+            Console.WriteLine(input);
+            // find the length of the string
+            Console.WriteLine(input.Length);
+            int stringLength = input.Length;
+            Console.WriteLine(stringLength);
+        }
+    }
+}
